73ebaaed5d9f3181690b657ef394c1effefd09bc
Parthu <parthudhanireddy@gmail.com>
2026-07-19 10:26:32 +0530

    feat: update RoleRequestDTO

--- src/main/java/com/fooddelivery/common/dto/identity/RoleRequestDTO.java
+++ src/main/java/com/fooddelivery/common/dto/identity/RoleRequestDTO.java
@@ -2,6 +2,8 @@ package com.fooddelivery.common.dto.identity;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,7 +14,11 @@ import lombok.NoArgsConstructor;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class RoleRequestDTO {
     @NotBlank
+    @Size(max = 50)
+    @Pattern(regexp = "^[A-Za-z0-9_\\-]+$")
     private String serviceName;
     @NotBlank
+    @Size(max = 50)
+    @Pattern(regexp = "^[A-Za-z0-9_]+$")
     private String roleName;
 }
