c132b962ae3a97c0e750a181468368e220a0759a
Parthu <parthudhanireddy@gmail.com>
2026-09-15 10:55:30 +0530

    fix: explicitly name DefaultMoneyAccessPolicy bean to moneyAccessPolicy to fix SpEL expression evaluation

--- common-web/src/main/java/com/fooddelivery/common/security/money/DefaultMoneyAccessPolicy.java
+++ common-web/src/main/java/com/fooddelivery/common/security/money/DefaultMoneyAccessPolicy.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
-@Component
+@Component("moneyAccessPolicy")
 public class DefaultMoneyAccessPolicy implements MoneyAccessPolicy {
 
     private final RestaurantServiceClient restaurantServiceClient;
